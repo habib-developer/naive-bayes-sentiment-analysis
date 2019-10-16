35e6c1a19cbf4e05751e3834b99cd4e8bb9ca321
Habib ul Rehman <mhabibulrehman@gmail.com>
2019-10-16 19:55:17 +0500

    Add project files.

---
+++ Naive Bayes Sentiment Analysis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Naive_Bayes_Sentiment_Analysis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
